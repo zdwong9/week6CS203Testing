47c872bb9c2c34c4de9671399fa6f39a1e015e77
Don Ta <donta@smu.edu.sg>
2022-09-08 14:50:30 +0800

    JPA initialization and Maven version

--- src/main/java/csd/week6/Week6Application.java
+++ src/main/java/csd/week6/Week6Application.java
@@ -2,14 +2,31 @@ package csd.week6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import csd.week6.book.*;
+import csd.week6.user.User;
+import csd.week6.user.UserRepository;
 
 @SpringBootApplication
 public class Week6Application {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(Week6Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Week6Application.class, args);
 
+        // JPA book repository init
+        BookRepository books = ctx.getBean(BookRepository.class);
+        System.out.println("[Add book]: " + books.save(new Book("Spring Security Fundamentals")).getTitle());
+        System.out.println("[Add book]: " + books.save(new Book("Gone With The Wind")).getTitle());
+
+        // JPA user repository init
+        UserRepository users = ctx.getBean(UserRepository.class);
+        BCryptPasswordEncoder encoder = ctx.getBean(BCryptPasswordEncoder.class);
+        System.out.println("[Add user]: " + users.save(
+            new User("admin", encoder.encode("goodpassword"), "ROLE_ADMIN")).getUsername());
+        
     }
     
 }
