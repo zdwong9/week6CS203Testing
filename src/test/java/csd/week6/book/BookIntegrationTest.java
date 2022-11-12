21d8d525222e73f686d97b8f7bed6527e43efed5
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-11-12 19:25:18 +0800

    add stuff

--- src/test/java/csd/week6/book/BookIntegrationTest.java
+++ src/test/java/csd/week6/book/BookIntegrationTest.java
@@ -182,6 +182,9 @@ class BookIntegrationTest {
 		Book book = new Book("A New Hope");
 
 		System.out.println("hellooo");
+		System.out.println("hellooo");
+		System.out.println("hellooo");
+		System.out.println("hellooo");
 
 		// arrange uri
 		URI uri = new URI(baseUrl + port + "/books/" + book.getId());
